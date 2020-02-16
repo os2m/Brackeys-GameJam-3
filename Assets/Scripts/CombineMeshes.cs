0891c032f3b8505303874108b2eb65aad1c1b280
devmarcstorm <accounts_mas@protonmail.com>
2020-02-17 00:35:59 +0100

    Combine Meshes
    - better collision
    - small changes to physics settings
    - add driving range models / textures
    - smaller changes to some models

---
+++ Assets/Scripts/CombineMeshes.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class CombineMeshes : MonoBehaviour
+{
+    public void Combine()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            if (!meshFilters[i].gameObject.CompareTag("NoCollision"))
+            {
+                combine[i].mesh = meshFilters[i].sharedMesh;
+                combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            }
+
+            i++;
+        }
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        transform.gameObject.SetActive(true);
+
+        DestroyImmediate(GetComponent<MeshCollider>());
+        MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = transform.GetComponent<MeshFilter>().mesh;
+        transform.GetComponent<MeshRenderer>().enabled = false;
+    }
+}
--- Assets/Scripts/PlaygroundManager.cs
+++ Assets/Scripts/PlaygroundManager.cs
@@ -7,13 +7,17 @@ public class PlaygroundManager : MonoBehaviour
     public GameObject normalPrefab;
     public GameObject badHolePrefab;
     public GameObject golfHolePrefab;
+    public GameObject bunkerPrefab;
     public List<GameObject> spawner;
     [Space(10)]
     public int badHoles = 16;
     public int golfHoles = 2;
+    public int bunkers = 6;
+    [Space(10)]
+    public Transform zRotator;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         GameObject[] spawnerArray = GameObject.FindGameObjectsWithTag("Spawner");
         for (int i = 0; i < spawnerArray.Length; i++)
@@ -26,9 +30,10 @@ public class PlaygroundManager : MonoBehaviour
             int index = Random.Range(0, (spawner.Count - 1));
 
             GameObject badHole = Instantiate(badHolePrefab, spawner[index].transform.position, Quaternion.Euler(-90f, 0, 0));
-            badHole.transform.SetParent(spawner[index].transform);
+            badHole.transform.SetParent(zRotator);
 
             spawner.Remove(spawner[index]);
+            Destroy(spawner[index]);
         }
 
         for (int i = 0; i < golfHoles; i++)
@@ -36,16 +41,32 @@ public class PlaygroundManager : MonoBehaviour
             int index = Random.Range(0, (spawner.Count - 1));
 
             GameObject golfHole = Instantiate(golfHolePrefab, spawner[index].transform.position, Quaternion.Euler(-90f, 0, 0));
-            golfHole.transform.SetParent(spawner[index].transform);
+            golfHole.transform.SetParent(zRotator);
 
             spawner.Remove(spawner[index]);
+            Destroy(spawner[index]);
+        }
+
+        for (int i = 0; i < bunkers; i++)
+        {
+            int index = Random.Range(0, (spawner.Count - 1));
+
+            GameObject bunker = Instantiate(bunkerPrefab, spawner[index].transform.position, Quaternion.Euler(-90f, 0, 0));
+            bunker.transform.SetParent(zRotator);
+
+            spawner.Remove(spawner[index]);
+            Destroy(spawner[index]);
         }
 
         for (int i = 0; i < spawner.Count; i++)
         {
             GameObject normal = Instantiate(normalPrefab, spawner[i].transform.position, Quaternion.Euler(-90f, 0, 0));
-            normal.transform.SetParent(spawner[i].transform);
+            normal.transform.SetParent(zRotator);
+
+            Destroy(spawner[i]);
         }
+
+        zRotator.GetComponent<CombineMeshes>().Combine();
     }
 
     // Update is called once per frame
