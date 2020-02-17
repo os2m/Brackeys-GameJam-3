1b9057ea992e4a0ef3da0e4d5704d1cc982011d0
devmarcstorm <accounts_mas@protonmail.com>
2020-02-17 22:31:14 +0100

    Revision of the playing field
    - The player now has a little more secure space in the corners.
    - changes to models and textures
    - show control buttons

---
+++ Assets/Scripts/HightlightOnKey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class HightlightOnKey : MonoBehaviour
+{
+    public KeyCode key;
+    public Sprite normal;
+    public Sprite hightlighted;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(key))
+            image.sprite = hightlighted;
+        else
+            image.sprite = normal;
+    }
+}
